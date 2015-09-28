23e9c115a08bbee72b2cd28ffdd85c9f824fd9ca
Marc Brown <marc@creativemess.com>
2015-09-27 22:58:07 -0400

    Project shell

---
+++ TagifaiTests/TagifaiTests.swift
@@ -0,0 +1,36 @@
+//
+//  TagifaiTests.swift
+//  TagifaiTests
+//
+//  Created by Marc Brown on 9/27/15.
+//  Copyright © 2015 creative mess. All rights reserved.
+//
+
+import XCTest
+@testable import Tagifai
+
+class TagifaiTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
